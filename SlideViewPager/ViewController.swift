b7be3b16a70ddad3894f62881dcc70fad64af8de
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2019-10-02 13:03:39 +0200

    Initial Commit

---
+++ SlideViewPager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideViewPager
+//
+//  Created by Mostafa Abd ElFatah on 10/2/19.
+//  Copyright © 2019 Mostafa Abd ElFatah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
